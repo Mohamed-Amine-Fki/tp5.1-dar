ffbce18ec5adf39d516d686bddd78e1ad71a0427
Mohamed-Amine-Fki <147945209+Mohamed-Amine-Fki@users.noreply.github.com>
2023-11-12 16:08:04 +0100

    Update ConversionServer.java

--- tp5.1/RMIConversion_Server/src/rmiServer/ConversionServer.java
+++ tp5.1/RMIConversion_Server/src/rmiServer/ConversionServer.java
@@ -1,10 +1,23 @@
 package rmiServer;
 
+import rmiService.ConversionImpl;
+import rmiService.IConversion;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
 public class ConversionServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099); //anniaire
+            ConversionImpl od = new ConversionImpl(); // Skeleton
+            System.out.println(od.toString());
+            Naming.rebind("rmi://localhost:1099/ConversionObject",od);
 
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
 }

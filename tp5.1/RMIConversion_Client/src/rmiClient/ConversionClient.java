39343bb457f3bc427df3eec724b33e3a9f10d7c3
Mohamed-Amine-Fki <147945209+Mohamed-Amine-Fki@users.noreply.github.com>
2023-11-12 16:02:51 +0100

    Update ConversionClient.java

--- tp5.1/RMIConversion_Client/src/rmiClient/ConversionClient.java
+++ tp5.1/RMIConversion_Client/src/rmiClient/ConversionClient.java
@@ -1,5 +1,18 @@
 package rmiClient;
 
-public class ConversionClient {
+import rmiService.IConversion;
 
+import java.rmi.Naming;
+
+public class ConversionClient {
+    public static void main(String[] args) {
+        try {
+            IConversion stub = (IConversion) Naming.lookup("rmi://localhost:1099/ConversionObject");
+                                //Naming.lookup return an instance of Remote
+            System.out.println(stub.convertirMontant(700.00));
+            System.out.println(stub.convertirMontant(80.00));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
